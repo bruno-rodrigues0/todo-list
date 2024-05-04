ff8e36d0d7e77d7798f12ecbd8c0638f5d6aeb14
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-05-04 12:11:22 -0300

    feat: get items by stats filter

--- src/server/controllers/todoList/getItems.ts
+++ src/server/controllers/todoList/getItems.ts
@@ -1,17 +1,60 @@
-import {Request, Response} from 'express'
+import { Request, Response } from "express";
+import { pool as mysql } from "../../database";
+import { StatusCodes } from "http-status-codes";
+import * as yup from "yup";
+import { validation } from "../../shared/middleware";
 
-type TStats = 'todo' | 'in process' | 'done'
+type TStats = "todo" | "in process" | "done";
 
-type TItem = {
-    nome: string,
-    stats: TStats,
+interface IFilter {
+    nome?: string;
+    stats?: TStats;
 }
 
+export const getItemValidation = validation((getSchema) => ({
+    query: getSchema<IFilter>(
+        yup.object().shape({
+            nome: yup.string(),
+            stats: yup
+                .string()
+                .oneOf(["done", "todo", "in process"])
+                .required(),
+        })
+    ),
+}));
 
+export const getItems = (
+    req: Request<unknown, unknown, unknown, IFilter>,
+    res: Response
+) => {
+    mysql.getConnection((error, conn) => {
+        if(error){
+            return res.status(StatusCodes.BAD_REQUEST).send({
+                error: error
+            })
+        }
 
-export const getItems = (req: Request, res: Response) => {
-    return res.send({
-        body: req.body,
-        message: 'added',
+        let response: unknown;
+
+        if(req.query.stats){
+            conn.query(
+                `SELECT * FROM item WHERE stats LIKE ?`,
+                [req.query.stats],
+                (error, result, filed) => {
+                    conn.release();
+                    if(error){
+                        return res.status(StatusCodes.BAD_REQUEST).send({
+                            error: error
+                        })
+                    }
+
+                    return res.status(StatusCodes.ACCEPTED).send({
+                        status: 'ACCEPTED',
+                        result: result,
+                        query: req.query
+                    })
+                }
+            )
+        }
     })
-}
+};
